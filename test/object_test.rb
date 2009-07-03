7b61008b872f7b57816c1cb713c7c9199f9b5741
Mirosław Boruta <mirek@synergypeople.net>
2009-07-03 16:57:50 +0200

    first object unit test

--- test/object_test.rb
+++ test/object_test.rb
@@ -4,4 +4,17 @@ class ObjectTest < Test::Unit::TestCase
   def setup
   end
 
+  def test_initilalize
+    assert_raise ArgumentError do S3::Object.new(nil, "") end # should not allow empty key
+    assert_raise ArgumentError do S3::Object.new(nil, "//") end # should not allow key with double slash
+  end
+
+  def test_full_key
+  end
+
+  def test_url
+  end
+
+  def test_cname_url
+  end
 end
