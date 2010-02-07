3e6c0632ef4e17f7a46eb3042fc405a162f068b0
Jakub Kuźma <qoobaa+github@gmail.com>
2010-02-07 14:19:27 +0100

    removed rr from test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,7 @@
 require "rubygems"
 require "test/unit"
-# require "rr"
 require "mocha"
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require "s3"
-
-# class Test::Unit::TestCase
-#   include RR::Adapters::TestUnit
-# end
