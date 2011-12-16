c0d7e6ca9f98a15b5fa059b24c5640626378b81e
Jakub Kuźma <qoobaa@gmail.com>
2011-12-16 14:41:42 +0100

    cleaned list_bucket

--- lib/s3/bucket.rb
+++ lib/s3/bucket.rb
@@ -112,20 +112,19 @@ module S3
 
     def list_bucket(options = {})
       response = bucket_request(:get, :params => options)
+      max_keys = options[:max_keys]
       objects_attributes = parse_list_bucket_result(response.body)
 
-      # If there are more than 1000 objects S3 truncates listing
-      # and we need to request another listing for the remaining objects.
+      # If there are more than 1000 objects S3 truncates listing and
+      # we need to request another listing for the remaining objects.
       while parse_is_truncated(response.body)
-      	next_request_options = {
-      		:marker => objects_attributes.last[:key]
-      	}
-      	
-      	if options[:max_keys]
-      		break if objects_attributes.length >= options[:max_keys]
-      		next_request_options[:max_keys] = options[:max_keys] - objects_attributes.length
-      	end
-      		
+        next_request_options = {:marker => objects_attributes.last[:key]}
+
+        if max_keys
+          break if objects_attributes.length >= max_keys
+          next_request_options[:max_keys] = max_keys - objects_attributes.length
+        end
+
         response = bucket_request(:get, :params => options.merge(next_request_options))
         objects_attributes += parse_list_bucket_result(response.body)
       end
